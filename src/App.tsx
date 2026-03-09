2e4615183ab5ff202fbc4c1876f953b225e22df3
WenKai Shi <73520560+eliajhmauve@users.noreply.github.com>
2026-03-09 21:52:39 +0800

    fix: add basename to Router for GitHub Pages subdirectory

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/app-...-44494674">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
